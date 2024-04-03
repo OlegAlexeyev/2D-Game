ae53f0ab82a49fb01063274d4b0d9005b27decb6
OlegAlexeyev <alexeyev.olegg@gmail.com>
2024-04-03 02:33:29 +0200

    Update TestScript1.cs

--- First Game Project/Assets/TestScript1.cs
+++ First Game Project/Assets/TestScript1.cs
@@ -7,7 +7,7 @@ public class TestScript1 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // newly added code here
     }
 
     // Update is called once per frame
